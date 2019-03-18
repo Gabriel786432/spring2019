66ffb0a7705fe76e9cd7b80094aa0326d4a4f11b
Gabriel786432 <43486098+Gabriel786432@users.noreply.github.com>
2019-03-17 21:28:56 -0400

    Create part2.py

---
+++ hw2/part2.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array = np.ones((3, 3, 3))
+x = 0
+
+for a in range(3):
+    for b in range(3):
+        for c in range(3):
+            array[a][b][c] = 5+x
+            x += 1
+
+print(array)
